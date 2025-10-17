3655359937379ce8c54d61334c1e7bda360c36e4
Augusto Melo <99753415+DevAugustomelo@users.noreply.github.com>
2025-10-17 01:52:22 +0000

    Updading file to training in github Codespaces.

--- alura/logica.py
+++ alura/logica.py
@@ -1,24 +1,24 @@
 from random import randint
 
-print('Boas vindas ao jogo do número secreto');
+print('Boas vindas ao jogo do número secreto')
 
-numero_secreto = randint(1, 10);
-print(numero_secreto);
-chute = None;
-tentativas = 1;
+numero_secreto = randint(1, 10)
+print(numero_secreto)
+chute = None
+tentativas = 1
 
 while chute != numero_secreto:
-    chute = int(input('Escolha um número entre 1 e 10:\n'));
+    chute = int(input('Escolha um número entre 1 e 10:\n'))
     if chute == numero_secreto:
-        break;
+        break
     else:
         if chute > numero_secreto:
-            print(f'O número secreto é menor que {chute}');
+            print(f'O número secreto é menor que {chute}')
         else:
-            print(f'O número secreto é maior que {chute}');
+            print(f'O número secreto é maior que {chute}')
             
         tentativas+=1
 
 
-palavra_correta = 'tentativas' if tentativas > 1  else 'tentativa';
+palavra_correta = 'tentativas' if tentativas > 1  else 'tentativa'
 print(f'Isso aí!!! Você descobriu o número secreto {numero_secreto} com {tentativas} {palavra_correta}')
